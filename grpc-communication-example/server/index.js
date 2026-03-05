a62ae0c7a05891d428ec5d1a9958753e007a66a2
Somnath Kar <somnathkar201901@gmail.com>
2026-03-05 23:14:07 +0530

    Added grpc server

--- grpc-communication-example/server/index.js
+++ grpc-communication-example/server/index.js
@@ -1,1 +1,36 @@
-console.log("Yes running");
+import grpc from "@grpc/grpc-js";
+import protoLoader from "@grpc/proto-loader";
+import path from "path";
+
+const PROTO_PATH = path.join(import.meta.dirname, "../customers.proto");
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  arrays: true,
+  longs: String,
+  enums: String,
+});
+
+const customersProto = grpc.loadPackageDefinition(packageDefinition);
+
+const server = new grpc.Server();
+
+server.addService(customersProto.CustomerService.service, {
+  getAll: (call, callBack) => {},
+  get: (call, callBack) => {},
+  insert: (call, callBack) => {},
+  update: (call, callBack) => {},
+  remove: (call, callBack) => {},
+});
+
+server.bindAsync(
+  "127.0.0.1:4001",
+  grpc.ServerCredentials.createInsecure(),
+  (error, port) => {
+    if (error) {
+      console.log("Got an error while starting the server ", error);
+      return;
+    }
+    console.log("Server is running on port: ", port);
+  }
+);
